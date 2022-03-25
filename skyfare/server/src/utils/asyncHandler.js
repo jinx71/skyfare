16661b7f051b64dc4408b86e824f081a4cc45fa4
jinx71 <karim.sazed@gmail.com>
2022-03-25 00:04:00 +0000

    feat: implement asyncHandler utility

---
+++ skyfare/server/src/utils/asyncHandler.js
@@ -0,0 +1,11 @@
+/**
+ * Wraps an async route handler so any rejected promise is forwarded to the
+ * centralized error-handling middleware instead of crashing the process.
+ *
+ * Usage: router.get('/', asyncHandler(async (req, res) => { ... }))
+ */
+const asyncHandler = (fn) => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next);
+};
+
+module.exports = asyncHandler;
